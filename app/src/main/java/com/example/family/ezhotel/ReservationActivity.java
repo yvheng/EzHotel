769cce64e941544cd5536bc1749bc20a44775aaa
Yv Heng <kongyh-wa15@student.tarc.edu.my>
2018-07-25 18:49:56 +0800

    reserve

---
+++ app/src/main/java/com/example/family/ezhotel/ReservationActivity.java
@@ -0,0 +1,13 @@
+package com.example.family.ezhotel;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ReservationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_reservation);
+    }
+}
